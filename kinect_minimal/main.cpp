c451813a23869f4af67fd7f19a8444b38381065b
Adam Fong <adamfong888@gmail.com>
2023-12-13 21:52:34 -0800

    Adding a minimal implementation of a program using libfreenect2. This should be a good basis to make a RGB and depth image publisher in noetic

---
+++ kinect_minimal/main.cpp
@@ -0,0 +1,87 @@
+#include <libfreenect2/libfreenect2.hpp>
+#include <libfreenect2/frame_listener_impl.h>
+#include <libfreenect2/registration.h>
+#include <libfreenect2/packet_pipeline.h>
+#include <libfreenect2/logger.h>
+#include <fstream>
+#include <cstdlib>
+#include <iostream>
+
+
+int main(){
+
+    libfreenect2::Freenect2 freenect2;
+    libfreenect2::Freenect2Device *dev = 0;
+    libfreenect2::PacketPipeline *pipeline = 0;
+
+    std::string serial = "";
+    bool enable_rgb = true;
+    bool enable_depth = true;
+    bool protonect_shutdown = false;
+
+    size_t framecount = 0;
+
+    int deviceId = -1;
+    size_t framemax = 15;
+    if(freenect2.enumerateDevices() == 0)
+    {
+        std::cout << "no device connected!" << std::endl;
+        return -1;
+    }
+    if (serial == "")
+    {
+        serial = freenect2.getDefaultDeviceSerialNumber();
+    }
+
+    pipeline = new libfreenect2::CudaPacketPipeline(0);
+    dev = freenect2.openDevice(serial, pipeline);
+    int types = 0;
+    if (enable_rgb){
+        types |= libfreenect2::Frame::Color;
+    }
+    if (enable_depth)
+        types |= libfreenect2::Frame::Ir | libfreenect2::Frame::Depth;
+    libfreenect2::SyncMultiFrameListener listener(types);
+    libfreenect2::FrameMap frames;
+    dev->setColorFrameListener(&listener);
+    dev->setIrAndDepthFrameListener(&listener);
+
+    if (enable_rgb && enable_depth)
+    {
+    if (!dev->start())ct_shutdown && (framemax == (size_t)-1 || framecount < framemax))
+    {
+        if (!listener.waitForNewFrame(frames, 10*1000)) // 10 sconds
+        {
+        std::cout << "timeout!" << std::endl;
+        return -1;
+        }
+        libfreenect2::Frame *rgb = frames[libfreenect2::Frame::Color];
+        libfreenect2::Frame *ir = frames[libfreenect2::Frame::Ir];
+        libfreenect2::Frame *depth = frames[libfreenect2::Frame::Depth];
+        registration->apply(rgb, depth, &undistorted, &registered);
+        listener.release(frames);
+        framecount++;
+    }
+    std::cout << "device serial: " << dev->getSerialNumber() << std::endl;
+    std::cout << "device firmware: " << dev->getFirmwareVersion() << std::endl;
+
+    libfreenect2::Registration* registration = new libfreenect2::Registration(dev->getIrCameraParams(), dev->getColorCameraParams());
+    libfreenect2::Frame undistorted(512, 424, 4), registered(512, 424, 4);
+
+    while(!protonect_shutdown && (framemax == (size_t)-1 || framecount < framemax))
+    {
+        if (!listener.waitForNewFrame(frames, 10*1000)) // 10 sconds
+        {
+        std::cout << "timeout!" << std::endl;
+        return -1;
+        }
+        libfreenect2::Frame *rgb = frames[libfreenect2::Frame::Color];
+        libfreenect2::Frame *ir = frames[libfreenect2::Frame::Ir];
+        libfreenect2::Frame *depth = frames[libfreenect2::Frame::Depth];
+        registration->apply(rgb, depth, &undistorted, &registered);
+        listener.release(frames);
+        framecount++;
+    }
+
+  return 0;
+}
