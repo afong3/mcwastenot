b2658f777e327f0341008c37032b04bad86ca0cb
Daniel J <80719727+dtaj@users.noreply.github.com>
2024-03-17 14:30:15 -0700

    Update main.cpp
    basic servo sweeping code

--- Code/ESP32/Servo_Test/src/main.cpp
+++ Code/ESP32/Servo_Test/src/main.cpp
@@ -1,26 +1,56 @@
 #include <ESP32Servo.h>
 
-Servo myservo;  // Create a servo object
+// Define Servo objects
+Servo myservo1; Servo myservo2; Servo myservo3; Servo myservo4;
+Servo myservo5; Servo myservo6; Servo myservo7; Servo myservo8;
 
-int servoPin = 4;  // GPIO pin where the servo is connected
-int angle = 0;      // Servo position in degrees
+// Define servo pins
+int servoPin1 = 3; int servoPin2 = 34; int servoPin3 = 9; int servoPin4 = 37;
+int servoPin5 = 36; int servoPin6 = 7;
+int angle = 85;
 
 void setup() {
-    // Calibrate the pulse width range to 600-2400 microseconds
-    myservo.attach(servoPin, 700, 2300);  
-    myservo.write(90); // Set to middle position
+  ESP32PWM::allocateTimer(0);
+  ESP32PWM::allocateTimer(1);
+  ESP32PWM::allocateTimer(2);
+  ESP32PWM::allocateTimer(3);
+  myservo1.setPeriodHertz(50);
+  
+  // Attach servos to their pins
+  myservo1.attach(servoPin1, 500, 2500);
+  myservo2.attach(servoPin2, 500, 2500);
+  myservo3.attach(servoPin3, 500, 2500);
+  myservo4.attach(servoPin4, 500, 2500);
+  myservo5.attach(servoPin5, 500, 2500);
+  myservo6.attach(servoPin6, 500, 2500);
+
+  myservo1.write(angle);
+  myservo2.write(angle);
+  myservo3.write(angle);
+  myservo4.write(angle);
+  myservo5.write(angle);
+  myservo6.write(angle);
+
+  pinMode(13, OUTPUT);
+  digitalWrite(13, LOW); // Initialize the LED state
 }
 
 void loop() {
-    // Sweep from 0 to 180 degrees
-    for (angle = 0; angle <= 180; angle += 1) {
-        myservo.write(angle);  // Tell servo to go to position in variable 'angle'
-        delay(15);             // Wait 15ms for the servo to reach the position
-    }
+  digitalWrite(13, HIGH); // LED on
+  delay(500); // Delay for half a second
+  digitalWrite(13, LOW); // LED off
+  delay(500); // Delay for half a second
 
-    // Sweep back from 180 to 0 degrees
-    for (angle = 180; angle >= 0; angle -= 1) {
-        myservo.write(angle);  
-        delay(15);             
+  if (angle < 180) {
+    for (angle = 0; angle <= 180; angle += 1) {
+      myservo1.write(angle);
+      myservo2.write(angle);
+      myservo3.write(angle);
+      myservo4.write(angle);
+      myservo5.write(angle);
+      myservo6.write(angle);
+      delay(30);
     }
+  }
+
 }
